30140f7f6edf780ef548689bb3a9a31669a29bab
anju2408 <anjudewangan2000@gmail.com>
2022-07-16 23:25:15 +0530

    Add header, section, aside-left

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Section from './components/Section';
+import AsideLeft from './components/AsideLeft';
 
 function App() {
   return (
    <>
    <Header />
    <Section />
+   <AsideLeft />
    </>
   );
 }
