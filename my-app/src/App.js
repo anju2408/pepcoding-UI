2183f795058da6ff1b152b531684d714400b47da
anju2408 <anjudewangan2000@gmail.com>
2022-07-16 22:34:16 +0530

    Add Header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Section from './components/Section';
 
 function App() {
   return (
    <>
    <Header />
+   <Section />
    </>
   );
 }
---
+++ my-app/src/components/Section.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './section.css';
+
+export default function Section(){
+    return(
+        <>
+        <div className="section">
+            <div className="form-section"></div>
+        </div>
+        </>
+    )
+}
